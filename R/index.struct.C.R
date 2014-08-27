26e1eb193a779b8fb56024a31c6ac63ace73a9c9
brodieG <brodieG@users.noreply.github.com>
2014-08-26 22:21:44 -0400

    recursion seems to be working in base case

--- R/index.struct.C.R
+++ R/index.struct.C.R
@@ -51,13 +51,14 @@ my_add4 <- cfunction(c(a = "integer", b = "integer"), "
   return R_NilValue;
 ")
 
-lst <- list(list(1,2), list(3, list(1, list(1, list(1, 2, 3)))))
-lst.2 <- list(list(1,2), list(3, list(1, list(1))))
+lst <-   list(list( 1,  2), list( 3, list( 4, list( 5, list(6, 6.1, 6.2)))))
+lst.2 <- list(list(11, 21), list(31, list(41, list(51, list(61          )))))
 library(inline)
 
 boom <- cfunction(c(target="list", current="list"), "
   int ind_val = 0;          /* current index value */
   int ind_lvl = 0;          /* how deep in the stack we are */
+  int ind_lvl_max = -1;     /* deepest we've gone in current stack */
   int ind_stk_sz = 32;      /* current size of stack */
   int ind_stk[ind_stk_sz];  /* track the stack */
   SEXP sxp_stk_tar[ind_stk_sz]; /* track the parent objects */
@@ -65,70 +66,63 @@ boom <- cfunction(c(target="list", current="list"), "
   int emr_brk = 0;
   int i, k=0;
 
-  SEXP tar_nxt = target;
-  SEXP cur_nxt = current;
+  sxp_stk_tar[0] = target;
+  sxp_stk_cur[0] = current;
 
   while(TRUE) {   
     
-    Rprintf(\"At Level %d, index %d, length %d, type %d, length.2 %d, type2 %d\\n\", ind_lvl, ind_val, length(tar_nxt), TYPEOF(tar_nxt), length(cur_nxt), TYPEOF(cur_nxt));
+    if(ind_lvl_max < ind_lvl) { /* need to initialize current stack position */
+      ind_stk[ind_lvl] = 0;
+      ind_lvl_max = ind_lvl;
+    }
+    if(ind_lvl >= ind_stk_sz) {
+      error(\"Exceeded Stack Size\");  /* placeholder, should re-allocate stack */
+    } 
+
+    /**************************************************************************/
+    if(ind_lvl < 0)
+      error(\"Logic Error, level drop not detected\");
+
+    Rprintf(\"At Level %d, index %d, length %d, type %d, length.2 %d, type2 %d\\n\", ind_lvl, ind_stk[ind_lvl], length(target), TYPEOF(target), length(current), TYPEOF(current));
+    for(i=0; i <= ind_lvl_max; i++) {
+      Rprintf(\"%d \", ind_stk[i]);
+    }
+    if(TYPEOF(target) == 14)
+      Rprintf(\"Value: %f\", REAL(target)[0]);
+    Rprintf(\"\\n\");
 
     if(emr_brk++ > 50) 
       error(\"Infininte Loop?\");
-
-    if(TYPEOF(tar_nxt) == 19) {
-      if(TYPEOF(cur_nxt) != 19 || length(cur_nxt) != length(tar_nxt)) {
-        /* SEXP chr_err;
-        chr_err = PROTECT(allocVector(STRSXP, 1)); */
-        int ind_len = 0;
-        int ind_sz = 0; 
-        int ind_sz_max = 0; 
-        
-        for(i = 1; i <= ind_lvl; i++) { /* first level is meaningless */
-          if((ind_sz = (int) ceil(log10(ind_stk[i] + 1.1))) > ind_sz_max)
-            ind_sz_max = ind_sz;  /* we will use to allocate a char vec of appropriate size */
-          /* Rprintf(\"  ind_sz: %d, ind_len: %d, stk: %d, log: %3.1f, ceil: %3.1f \\n\", ind_sz, ind_len, ind_stk[i] + 1, log10(ind_stk[i] + 1), ceil(log10(ind_stk[i] + 1))); */
-          ind_len = ind_len + ind_sz;
-        }
-        /* Rprintf(\"ind_sz_max: %d, ind_len: %d\\n\", ind_sz_max, ind_len); */
-        char chr_err_all[ind_len + 4 * (ind_lvl + 1) + 1];
-        char chr_err[ind_sz_max + 4 + 1];
-        chr_err_all[0] = chr_err[0] = '\\0';
-        for(i = 1; i <= ind_lvl; i++) {
-          /* Rprintf(\"Doing: [[%d]]\", ind_stk[i] + 1); */
-          sprintf(chr_err, \"[[%d]]\", ind_stk[i] + 1);
-          strcat(chr_err_all, chr_err);
-        }
-        Rprintf(\"%s\\n\", chr_err_all);
-        return R_NilValue;
-
-        error(\"Structure Mismatch\");
-      }
-      if(length(tar_nxt) > ind_val) {      /* if this isn't TRUE then we need to drop down a level? */
-        if(ind_lvl >= ind_stk_sz) {
-          error(\"Exceeded Stack Size\");  /* placeholder, should re-allocate stack */
-        } else if (ind_lvl < 0) {
-          error(\"Negative Stack Index\"); /* genuine error */
-        }
-        ind_stk[ind_lvl] = ind_val;
-        sxp_stk_tar[ind_lvl] = tar_nxt;
-        sxp_stk_cur[ind_lvl] = cur_nxt;
-        tar_nxt = VECTOR_ELT(tar_nxt, ind_val);
-        cur_nxt = VECTOR_ELT(cur_nxt, ind_val);
-        ind_lvl++;
-        ind_stk[ind_lvl] = ind_val = 0;        
-      } else {
-        if(--ind_lvl < 0)
+    if (ind_lvl < 0) {
+      error(\"Negative Stack Index\"); /* genuine error */
+    }
+    /**************************************************************************/
+    
+    if(TYPEOF(target) == 19) {
+      if(ind_stk[ind_lvl] + 1 > length(target)) { /* no sub-items to check */
+        if(ind_lvl <= 0)
           break;
-        ind_val = ind_stk[ind_lvl] + 1;
-        tar_nxt = sxp_stk_tar[ind_lvl];
-        cur_nxt = sxp_stk_tar[ind_lvl];
+        target = sxp_stk_tar[ind_lvl - 1];
+        /* current = sxp_stk_cur[ind_lvl - 1]; */
+        ind_stk[ind_lvl - 1]++;     /* Since we completed list, parent pointer should be moved forward to review next */
+        ind_stk[ind_lvl] = 0;       /* Need to reset pointer before we leave this level as next time we get here it will be with a fresh list */
+        ind_lvl--;
+        ind_lvl_max--;
+      } else {
+        sxp_stk_tar[ind_lvl] = target;
+        sxp_stk_cur[ind_lvl] = current;
+        Rprintf(\"At level %d, advancing to %d\\n\", ind_lvl, ind_stk[ind_lvl]);
+        target = VECTOR_ELT(target, ind_stk[ind_lvl]);
+        /* current = VECTOR_ELT(current, ind_stk[ind_lvl]); */
+        ind_lvl++;
       }
     } else {
-      if(--ind_lvl < 0)
+      if(ind_lvl <= 0)
         break;
-      ind_val = ind_stk[ind_lvl] + 1;
-      tar_nxt = sxp_stk_tar[ind_lvl];
-      cur_nxt = sxp_stk_tar[ind_lvl];
+      target = sxp_stk_tar[ind_lvl - 1];
+      /* current = sxp_stk_cur[ind_lvl - 1]; */
+      ind_stk[ind_lvl - 1]++;
+      ind_lvl--;
     }
   }
   /* Rprintf(\"%f\", type2char(TYPEOF(a))); */
@@ -145,4 +139,37 @@ boom <- cfunction(c(target="list", current="list"), "
   return res;
 ")
 
+
 boom(lst, lst.2)
+
+      # if(TYPEOF(cur_nxt) != 19 || length(cur_nxt) != length(tar_nxt)) {
+      #   /* SEXP chr_err;
+      #   chr_err = PROTECT(allocVector(STRSXP, 1)); */
+      #   if(TYPEOF(cur_nxt) != 19)
+      #     Rprintf(\"List mismatch failure\\n\");
+      #   if(length(cur_nxt) != length(tar_nxt))
+      #     Rprintf(\"Length mismatch failure\\n\");
+      #   int ind_len = 0;
+      #   int ind_sz = 0; 
+      #   int ind_sz_max = 0; 
+        
+      #   for(i = 0; i <= ind_lvl; i++) { /* first level is meaningless; not sure */
+      #     if((ind_sz = (int) ceil(log10(ind_stk[i] + 1.1))) > ind_sz_max)
+      #       ind_sz_max = ind_sz;  /* we will use to allocate a char vec of appropriate size */
+      #     /* Rprintf(\"  ind_sz: %d, ind_len: %d, stk: %d, log: %3.1f, ceil: %3.1f \\n\", ind_sz, ind_len, ind_stk[i] + 1, log10(ind_stk[i] + 1), ceil(log10(ind_stk[i] + 1))); */
+      #     ind_len = ind_len + ind_sz;
+      #   }
+      #   /* Rprintf(\"ind_sz_max: %d, ind_len: %d\\n\", ind_sz_max, ind_len); */
+      #   char chr_err_all[ind_len + 4 * (ind_lvl + 1) + 1];
+      #   char chr_err[ind_sz_max + 4 + 1];
+      #   chr_err_all[0] = chr_err[0] = '\\0';
+      #   for(i = 0; i <= ind_lvl; i++) {
+      #     /* Rprintf(\"Doing: [[%d]]\", ind_stk[i] + 1); */
+      #     sprintf(chr_err, \"[[%d]]\", ind_stk[i] + 1);
+      #     strcat(chr_err_all, chr_err);
+      #   }
+      #   Rprintf(\"%s\\n\", chr_err_all);
+      #   return R_NilValue;
+
+      #   error(\"Structure Mismatch\");
+      # }
