409a057382926a424d3f5f7a4db8b62400f71fec
brodieG <brodieG@users.noreply.github.com>
2014-09-29 22:58:58 -0400

    test updates

--- tests/unitizer/alike.R
+++ tests/unitizer/alike.R
@@ -144,6 +144,18 @@ unitizer_sect("S4", {
 
 
 } )
+unitizer_sect("R5", {
+  Foo <- setRefClass("Foo")
+  Bar <- setRefClass("Bar")
+
+  Foo.1 <- Foo$new()
+  Foo.2 <- Foo$new()
+  Bar.1 <- Bar$new()
+
+  alike(Foo.1, Foo.2)
+  alike(Foo.1, Bar.1)
+
+} ) 
 unitizer_sect("Non-Standard Class", {
   # Basically ensure that stuff still recurses even if they are lists/calls
   # but have another class
@@ -203,4 +215,75 @@ unitizer_sect("data.table", {
   alike(dt.1, dt.2)
   alike(dt.1, dt.3)
   alike(dt.3, dt.1)
+} )
+unitizer_sect("Errors", {
+  alike(1, 1, int.mode="hello")
+  alike(1, 1, int.mode=3)
+  alike(1, 1, int.tol="hello")
+  alike(1, 1, attr.mode=3)
+} )
+# Subset of tests for "fast" version
+
+unitizer_sect(".alike", {
+  .alike(integer(), 1:3)    # TRUE
+  .alike(integer(5L), 1:3)  # FALSE
+  .alike(integer(3L), 1:3)  # TRUE
+  .alike(integer(), 1:3, int.mode=1L)         # Error, this arg isn't available
+  .alike(letters[1:4], c("hello", "goodbye", "ba", "da"))  # TRUE
+
+  .alike(lst, lst.2)     # length mismatch
+  .alike(lst, lst.3)     # object type mismatch
+  .alike(1:10, "hello")  # object type mismatch, no dive
+  .alike(matrix(integer(), ncol=7), matrix(1:21, nrow=3))
+  .alike(matrix(integer(), ncol=3, dimnames=list(NULL, c("R", "G", "B"))), matrix(1:21, ncol=3, dimnames=list(NULL, c("R", "G", "B"))))
+} )
+# These are also part of the examples, but here as well so that issues are 
+# detected during development and not the last minute package checks
+
+unitizer_sect("Examples", {
+  alike(1L, 1.0)         # TRUE, because 1.0 is integer-like
+  alike(1L, 1.1)         # FALSE, 1.1 is not integer-like
+  alike(1.1, 1L)         # TRUE, by default, integers are always considered real
+  alike(integer(), 1:4)  # TRUE, Zero length `target` matches any length `current`
+  alike(1:4, integer())  # But not vice versa
+
+  # Ever notice how annoying it is to test for "scalarness", consider:
+
+  x <- 1
+  x.2 <- 1:3
+  y <- TRUE
+  y.2 <- c(TRUE, TRUE)
+
+  alike(integer(1L), x)
+  alike(logical(1L), y)
+  alike(integer(1L), x.2)
+  alike(logical(1L), y.2)
+
+  # `alike` will compare data frame columns
+  
+  df.tpl <- data.frame(id=integer(), grade=factor(levels=LETTERS[1:6]))
+  df.cur <- data.frame(id=c(1, 3, 5), grade=factor(c("A", "F", "B"), levels=LETTERS[1:6]))
+  df.cur2 <- data.frame(id=c(1, 3, 5), grade=c("A", "F", "B"))
+  
+  alike(df.tpl, df.cur)    # zero row df as `target` matches any length df
+  alike(df.cur, df.tpl)    # alike is not "commutative", now `target` is not zero row
+  
+  # factor levels must match; makes sense, otherwise it really isn't the same 
+  # type of data (note this is a recursive comparison); for better understanding
+  # of error examine `levels(df.tpl[[2]])` and `levels(df.cur2[[2]])`
+
+  alike(df.tpl, df.cur2)   
+  
+  alike(list(integer(), df.tpl), list(1:4, df.cur))  # recursive comparison
+  alike(matrix(integer(), 3), matrix(1:21, ncol=7))  # partially specified dimensions
+  
+  # In order for objects to be alike, they must share a family tree, not just
+  # a common class
+  
+  obj.tpl <- structure(TRUE, class=letters[1:3])
+  obj.cur.1 <-  structure(TRUE, class=c("x", letters[1:3]))
+  obj.cur.2 <-  structure(TRUE, class=c(letters[1:3], "x"))
+  
+  alike(obj.tpl, obj.cur.1)
+  alike(obj.tpl, obj.cur.2) 
 } )
