39c93d760ce47e3063a115249d8e566521391cd2
brodieG <brodieG@users.noreply.github.com>
2015-01-31 21:53:43 -0500

    simplifying lang error message

--- src/lang.c
+++ src/lang.c
@@ -45,13 +45,11 @@ char * ALIKEC_symb_abstract(SEXP symb, pfHashTable * hash, size_t * varnum) {
   return symb_abs;
 }
 
-const char * ALIKEC_lang_alike_rec(
+int ALIKEC_lang_alike_rec(
   SEXP target, SEXP current, pfHashTable * tar_hash, pfHashTable * cur_hash,
   size_t * tar_varnum, size_t * cur_varnum, int formula
 ) {
-  if(CAR(target) != CAR(current)) {  // Actual fun call must match exactly
-    return "language mismatch 1";
-  }
+  if(CAR(target) != CAR(current)) return 0;  // Actual fun call must match exactly
   SEXP tar_sub, cur_sub;
   for(
     tar_sub = CDR(target), cur_sub = CDR(current); tar_sub != R_NilValue;
@@ -62,15 +60,14 @@ const char * ALIKEC_lang_alike_rec(
 
     SEXP tar_sub_car = CAR(tar_sub), cur_sub_car = CAR(cur_sub);
     SEXPTYPE tsc_type = TYPEOF(tar_sub_car), csc_type = TYPEOF(cur_sub_car);
-    if((tsc_type == SYMSXP || csc_type == SYMSXP) && tsc_type != csc_type) {
-      return "language mismach 2";
-    }
+    if((tsc_type == SYMSXP || csc_type == SYMSXP) && tsc_type != csc_type)
+      return 0;
     if(tsc_type == SYMSXP) {
       char * tar_abs = ALIKEC_symb_abstract(tar_sub_car, tar_hash, tar_varnum);
       char * cur_abs = ALIKEC_symb_abstract(cur_sub_car, cur_hash, cur_varnum);
-      if(!strcmp(tar_abs, cur_abs)) return "language mismatch 3";
+      if(strcmp(tar_abs, cur_abs)) return 0;
     } else if (tsc_type == LANGSXP && csc_type != LANGSXP) {
-      return "language mismatch 4";
+      return 0;
     } else if (tsc_type == LANGSXP) {
       return ALIKEC_lang_alike_rec(
         tar_sub_car, cur_sub_car, tar_hash, cur_hash, tar_varnum,
@@ -80,10 +77,10 @@ const char * ALIKEC_lang_alike_rec(
       // Maybe this shouldn't be "identical", but too much of a pain in the butt
       // to do an all.equals type comparison
 
-      return "language mismatch 5";
+      return 0;
     }
   }
-  return "";
+  return 1;
 }
 /*
 Determine whether objects should be compared as calls or as formulas; the main
@@ -111,9 +108,17 @@ const char * ALIKEC_lang_alike_internal(SEXP target, SEXP current) {
   ) {
     formula = 1;
   }
-  return ALIKEC_lang_alike_rec(
+  int res = ALIKEC_lang_alike_rec(
     target, current, tar_hash, cur_hash, tar_varnum, cur_varnum, formula
   );
+  const char * err_msg = "";
+  if(!res) {
+    if(formula)
+      err_msg = "have matching formulas (see docs for what constitutes matching formulas)";
+    else
+      err_msg = "have matching calls (see docs for what constitute matching calls)";
+  }
+  return err_msg;
 }
 
 SEXP ALIKEC_lang_alike_ext(SEXP target, SEXP current) {
