5eff166a554685703dabc904a4ac0106f7d4e5c0
brodieG <brodieG@users.noreply.github.com>
2015-02-20 21:41:59 -0500

    whitespace

--- tests/unitizer/alike.R
+++ tests/unitizer/alike.R
@@ -262,58 +262,58 @@ unitizer_sect(".alike", {
 # detected during development and not the last minute package checks
 
 unitizer_sect("Examples", {
- alike(1L, 1.0)         # TRUE, because 1.0 is integer-like
- alike(1L, 1.1)         # FALSE, 1.1 is not integer-like
- alike(1.1, 1L)         # TRUE, by default, integers are always considered real
- alike(integer(), 1:4)  # TRUE, Zero length `target` matches any length `current`
- alike(1:4, integer())  # But not vice versa
+  alike(1L, 1.0)         # TRUE, because 1.0 is integer-like
+  alike(1L, 1.1)         # FALSE, 1.1 is not integer-like
+  alike(1.1, 1L)         # TRUE, by default, integers are always considered real
+  alike(integer(), 1:4)  # TRUE, Zero length `target` matches any length `current`
+  alike(1:4, integer())  # But not vice versa
 
- # Scalarness can now be checked at same time as type
+  # Scalarness can now be checked at same time as type
 
- x <- 1
- x.2 <- 1:3
- y <- TRUE
- y.2 <- c(TRUE, TRUE)
+  x <- 1
+  x.2 <- 1:3
+  y <- TRUE
+  y.2 <- c(TRUE, TRUE)
 
- alike(integer(1L), x)
- alike(logical(1L), y)
- alike(integer(1L), x.2)
- alike(logical(1L), y.2)
+  alike(integer(1L), x)
+  alike(logical(1L), y)
+  alike(integer(1L), x.2)
+  alike(logical(1L), y.2)
 
- # Zero length match any length of same type
+  # Zero length match any length of same type
 
- alike(integer(), 1:10)
+  alike(integer(), 1:10)
 
- # NULL matches anything
+  # NULL matches anything
 
- alike(NULL, mtcars)
- alike(list(NULL, NULL), list(iris, mtcars))
+  alike(NULL, mtcars)
+  alike(list(NULL, NULL), list(iris, mtcars))
 
- # `alike` will compare data frame columns
+  # `alike` will compare data frame columns
 
- df.tpl <- data.frame(id=integer(), grade=factor(levels=LETTERS[1:6]))
- df.cur <- data.frame(id=c(1, 3, 5), grade=factor(c("A", "F", "B"), levels=LETTERS[1:6]))
- df.cur2 <- data.frame(id=c(1, 3, 5), grade=c("A", "F", "B"))
+  df.tpl <- data.frame(id=integer(), grade=factor(levels=LETTERS[1:6]))
+  df.cur <- data.frame(id=c(1, 3, 5), grade=factor(c("A", "F", "B"), levels=LETTERS[1:6]))
+  df.cur2 <- data.frame(id=c(1, 3, 5), grade=c("A", "F", "B"))
 
- alike(df.tpl, df.cur)    # zero row df as `target` matches any length df
- alike(df.cur, df.tpl)    # alike is not "commutative", now `target` is not zero row
+  alike(df.tpl, df.cur)    # zero row df as `target` matches any length df
+  alike(df.cur, df.tpl)    # alike is not "commutative", now `target` is not zero row
 
- # factor levels must match; makes sense, otherwise it really isn't the same
- # type of data (note this is a recursive comparison); for better understanding
- # of error examine `levels(df.tpl[[2]])` and `levels(df.cur2[[2]])`
+  # factor levels must match; makes sense, otherwise it really isn't the same
+  # type of data (note this is a recursive comparison); for better understanding
+  # of error examine `levels(df.tpl[[2]])` and `levels(df.cur2[[2]])`
 
- alike(df.tpl, df.cur2)
+  alike(df.tpl, df.cur2)
 
- alike(list(integer(), df.tpl), list(1:4, df.cur))  # recursive comparison
- alike(matrix(integer(), 3), matrix(1:21, ncol=7))  # partially specified dimensions
+  alike(list(integer(), df.tpl), list(1:4, df.cur))  # recursive comparison
+  alike(matrix(integer(), 3), matrix(1:21, ncol=7))  # partially specified dimensions
 
- # In order for objects to be alike, they must share a family tree, not just
- # a common class
+  # In order for objects to be alike, they must share a family tree, not just
+  # a common class
 
- obj.tpl <- structure(TRUE, class=letters[1:3])
- obj.cur.1 <-  structure(TRUE, class=c("x", letters[1:3]))
- obj.cur.2 <-  structure(TRUE, class=c(letters[1:3], "x"))
+  obj.tpl <- structure(TRUE, class=letters[1:3])
+  obj.cur.1 <-  structure(TRUE, class=c("x", letters[1:3]))
+  obj.cur.2 <-  structure(TRUE, class=c(letters[1:3], "x"))
 
- alike(obj.tpl, obj.cur.1)
- alike(obj.tpl, obj.cur.2)
+  alike(obj.tpl, obj.cur.1)
+  alike(obj.tpl, obj.cur.2)
 } )
