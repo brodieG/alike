635a4597109f5c7452988a2338327ab92226cce1
brodieG <brodieG@users.noreply.github.com>
2017-02-14 21:16:57 -0500

    new return structure working

--- src/misc.c
+++ src/misc.c
@@ -427,10 +427,15 @@ Convert convention of zero length string == TRUE to SEXP
 */
 
 SEXP ALIKEC_string_or_true(struct ALIKEC_res_fin res) {
-  if(res.actual[0]) {
+  if(res.actual[0] && res.target[0]) {
     const char * res_str = CSR_smprintf4(
       ALIKEC_MAX_CHAR,
       "%sshould %s (%s)", res.call, res.actual, res.target, ""
+    );
+    return(mkString(res_str));
+  } else if (res.actual[0]) {
+    const char * res_str = CSR_smprintf4(
+      ALIKEC_MAX_CHAR, "%sshould %s", res.call, res.actual, "", ""
     );
     return(mkString(res_str));
   }
